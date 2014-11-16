e829d717b6af25908ab8dad2f59d7bf1b2420bae
Matt Dee <mattdee123@gmail.com>
2014-11-16 06:19:09 -0500

    added the layout

--- app/src/main/java/com/example/mattdee/eyes/MainActivity.java
+++ app/src/main/java/com/example/mattdee/eyes/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.mattdee.eyes;
 
 import android.app.Activity;
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.text.format.Formatter;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.apache.http.impl.DefaultConnectionReuseStrategy;
 import org.apache.http.impl.DefaultHttpResponseFactory;
@@ -21,11 +26,23 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+
+//        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+//        PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK, "My Tag");
+//        wl.acquire();
+
+
+        setContentView(R.layout.images);
         WifiManager wifi = (WifiManager) getSystemService(WIFI_SERVICE);
         String ip = Formatter.formatIpAddress(wifi.getConnectionInfo().getIpAddress());
-        TextView tv = (TextView) findViewById(R.id.tv);
-        tv.setText(ip);
+//        TextView tv = (TextView) findViewById(R.id.tv);
+//        tv.setText(ip);
+        Toast.makeText(getApplicationContext(), "Ip = " + ip, Toast.LENGTH_LONG).show();
         Log.d("TAG", "doing it");
 
         HttpProcessor httpproc = new BasicHttpProcessor();
